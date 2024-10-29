b6d244b03c0225c88fcf5b1d907769dc991fef3c
OSwego <syava23223@gmail.com>
2024-10-29 21:44:45 +0200

    Add Miles to Km converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,14 @@ package app;
 public class Main {
     public static void main (String[] args) {
         System.out.println("My Git app for Users");
+
+        double a = 8;
+        double b = 18;
+        double c = milTokm(a);
+        System.out.println(c);
+    }
+
+    private static double milTokm (double a) {
+        return a * 1.609;
     }
 }
